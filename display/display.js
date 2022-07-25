99f8f46c27e2480bdb2642b543768c74bcf51e93
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2022-07-25 17:51:55 +0200

    now only using 1 canvas to display all 4 images in trapezes

--- display/display.js
+++ display/display.js
@@ -1,25 +1,28 @@
-var canvases = [];
+var canvas;
+var spacingSlider = document.getElementById('spacingSlider');
+var spacingValueShow = document.getElementById('spacingValue');
+var imageZoomSlider = document.getElementById('imageZoom');
+var imagePosSlider = document.getElementById('imagePosition');
+var lastLoadedImage = new Image();
 window.onload = function () {
-    //add canvases to canvases-array in mathmaticaly positiv direction of rotation (clockwise) and then middleCanvas
-    canvases.push(document.getElementById('topCanvas'));
-    canvases.push(document.getElementById('rightCanvas'));
-    canvases.push(document.getElementById('bottomCanvas'));
-    canvases.push(document.getElementById('leftCanvas'));
-    canvases.push(document.getElementById('middleCanvas'));
-    //paint all canvases black
-    canvases.forEach(function (canvas) {
-        //try to load context
-        var context = canvas.getContext("2d");
-        if (!context) {
-            console.error("Canvas-context not supported!");
-            return;
-        }
-        //fill the current canvas black
-        context.fillStyle = "#000000";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-    });
-    //from now on we will work with only the four outer canvases
-    canvases.pop();
+    //----- prepare the canvas -----
+    if (document.getElementById('mainCanvas') == null) {
+        console.error('Can not find canvas!');
+        return;
+    }
+    //get the canvas
+    canvas = document.getElementById('mainCanvas');
+    //paint the canvas black
+    var context = canvas.getContext("2d");
+    if (!context) {
+        console.error("Canvas-context not supported!");
+        return;
+    }
+    //fill the canvas black
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    //----- prepare other things -----
+    spacingValueShow.value = "" + getInnerSpacing();
 };
 //onchange-event-listener for the image upload button
 function loadImage(event) {
@@ -43,29 +46,75 @@ function loadImage(event) {
         image.onload = function (imageEvent) {
             drawImageToCanvases(image);
         };
+        lastLoadedImage = image;
     };
     filereader.readAsDataURL(file);
 }
 function drawImageToCanvases(image) {
-    //draw the image on each canvas
-    canvases.forEach(function (canvas, index) {
-        //try to load context
-        var context = canvas.getContext("2d");
-        if (!context) {
-            console.error("Canvas-context not supported!");
-            return;
-        }
-        //calculate the rotation angle
-        var rotationAngle = (index * Math.PI) / 2;
-        //set width and height
-        var width = canvas.width, height = canvas.height;
-        console.log(width);
-        console.log(height);
-        console.log();
-        //rotate and then draw the image
-        context.translate(canvas.width / 2, canvas.height / 2);
-        context.rotate(rotationAngle);
-        context.translate(-canvas.width / 2, -canvas.height / 2);
-        context.drawImage(image, 0, 0, width, height);
-    });
+    if (image === void 0) { image = lastLoadedImage; }
+    var context = canvas.getContext("2d");
+    if (context == null)
+        return;
+    //get the variables
+    var innerSpacing = getInnerSpacing(), imagePositionOffset = getImagePosition(), singleCanvasWidth = canvas.width, singleCanvasHeight = canvas.height / 2 - innerSpacing / 2, imageSize = getImageZoom(), totalCanvasSize = canvas.width;
+    //clear the canvas
+    resetCanvasContext(context, totalCanvasSize);
+    //draw the images
+    for (var i = 0; i < 4; i++) {
+        //start drawing
+        context.save();
+        //rotate around center
+        context.translate(totalCanvasSize / 2, totalCanvasSize / 2);
+        context.rotate(i * Math.PI / 2);
+        context.translate(-totalCanvasSize / 2, -totalCanvasSize / 2);
+        //create the clipping mask
+        var clipMask = new Path2D();
+        clipMask.moveTo(0, 0);
+        clipMask.lineTo(singleCanvasWidth, 0);
+        clipMask.lineTo(singleCanvasWidth / 2 + innerSpacing / 2, singleCanvasHeight);
+        clipMask.lineTo(singleCanvasWidth / 2 - innerSpacing / 2, singleCanvasHeight);
+        clipMask.lineTo(0, 0);
+        clipMask.closePath();
+        context.clip(clipMask);
+        //move outwards
+        context.translate(0, singleCanvasHeight / 2 - imageSize / 2);
+        //draw the image
+        context.drawImage(image, singleCanvasWidth / 2 - imageSize / 2, singleCanvasHeight / 2 - imageSize / 2 + imagePositionOffset, imageSize, imageSize);
+        //end drawing
+        context.restore();
+    }
 }
+function resetCanvasContext(context, canvasSize) {
+    context.clearRect(0, 0, canvasSize, canvasSize);
+    context.beginPath();
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, canvasSize, canvasSize);
+}
+function getInnerSpacing() {
+    if (spacingSlider == null)
+        return 200;
+    return Number(spacingSlider.value);
+}
+spacingSlider.addEventListener('input', function (event) {
+    if (spacingValueShow)
+        spacingValueShow.value = "" + getInnerSpacing();
+    else
+        console.log("no textfield found");
+    drawImageToCanvases();
+});
+function getImageZoom() {
+    if (!imageZoomSlider)
+        return 600;
+    return Number(imageZoomSlider.value);
+}
+imageZoomSlider.addEventListener('input', function (event) {
+    drawImageToCanvases();
+});
+function getImagePosition() {
+    if (!imagePosSlider)
+        return 0;
+    return Number(imagePosSlider.value);
+}
+imagePosSlider.addEventListener('input', function (event) {
+    drawImageToCanvases();
+});
--- display/display.ts
+++ display/display.ts
@@ -1,30 +1,34 @@
-let canvases:HTMLCanvasElement[] = []
+let canvas:HTMLCanvasElement;
+
+const spacingSlider:HTMLInputElement = document.getElementById('spacingSlider') as HTMLInputElement;
+const spacingValueShow:HTMLInputElement = document.getElementById('spacingValue') as HTMLInputElement;
+const imageZoomSlider:HTMLInputElement = document.getElementById('imageZoom') as HTMLInputElement;
+const imagePosSlider:HTMLInputElement = document.getElementById('imagePosition') as HTMLInputElement;
+let lastLoadedImage:HTMLImageElement = new Image();
 
 window.onload = function() {
-    //add canvases to canvases-array in mathmaticaly positiv direction of rotation (clockwise) and then middleCanvas
-    canvases.push(document.getElementById('topCanvas') as HTMLCanvasElement);
-    canvases.push(document.getElementById('rightCanvas') as HTMLCanvasElement);
-    canvases.push(document.getElementById('bottomCanvas') as HTMLCanvasElement);
-    canvases.push(document.getElementById('leftCanvas') as HTMLCanvasElement);
-    canvases.push(document.getElementById('middleCanvas') as HTMLCanvasElement)
+    //----- prepare the canvas -----
+    if(document.getElementById('mainCanvas') == null) {
+        console.error('Can not find canvas!');
+        return;
+    }
 
-    //paint all canvases black
-    canvases.forEach((canvas) => {
+    //get the canvas
+    canvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
 
-        //try to load context
-        let context = canvas.getContext("2d");
-        if(!context) {
-            console.error("Canvas-context not supported!");
-            return;
-        }
+    //paint the canvas black
+    let context = canvas.getContext("2d");
+    if(!context) {
+        console.error("Canvas-context not supported!");
+        return;
+    }
 
-        //fill the current canvas black
-        context.fillStyle = "#000000";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-    });
+    //fill the canvas black
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-    //from now on we will work with only the four outer canvases
-    canvases.pop();
+    //----- prepare other things -----
+    spacingValueShow.value = "" + getInnerSpacing();
 }
 
 //onchange-event-listener for the image upload button
@@ -53,37 +57,101 @@ function loadImage(event:Event) {
         image.onload = function(imageEvent) {
             drawImageToCanvases(image);
         };
+
+        lastLoadedImage = image;
     }
 
     filereader.readAsDataURL(file);    
 }
 
-function drawImageToCanvases(image:HTMLImageElement) {
-    //draw the image on each canvas
-    canvases.forEach((canvas, index) => {
-        //try to load context
-        let context = canvas.getContext("2d");
-        if(!context) {
-            console.error("Canvas-context not supported!");
-            return;
-        }
+function drawImageToCanvases(image:HTMLImageElement = lastLoadedImage) {
+    const context = canvas.getContext("2d");
 
-        //calculate the rotation angle
-        let rotationAngle = (index * Math.PI) / 2;
+    if(context == null) return;
 
-        //set width and height
-        let width = canvas.width, height = canvas.height;
-        console.log(width);
-        console.log(height);
-        console.log();
-        
-        
-        
-        //rotate and then draw the image
-        context.translate(canvas.width/2, canvas.height/2);
-        context.rotate(rotationAngle);
-        context.translate(-canvas.width/2, -canvas.height/2);
+    //get the variables
+    const innerSpacing = getInnerSpacing(),
+        imagePositionOffset = getImagePosition(),
+        singleCanvasWidth = canvas.width,
+        singleCanvasHeight = canvas.height/2 - innerSpacing/2,
+        imageSize = getImageZoom(),
+        totalCanvasSize = canvas.width;
 
-        context.drawImage(image, 0, 0, width, height);
-    });
+    //clear the canvas
+    resetCanvasContext(context, totalCanvasSize);
+
+    //draw the images
+    for(let i = 0; i < 4; i++) {
+        //start drawing
+        context.save();
+
+        //rotate around center
+        context.translate(totalCanvasSize/2, totalCanvasSize/2);
+        context.rotate(i * Math.PI / 2);
+        context.translate(-totalCanvasSize/2, -totalCanvasSize/2);
+
+        //create the clipping mask
+        const clipMask = new Path2D();
+        clipMask.moveTo(0, 0);
+        clipMask.lineTo(singleCanvasWidth, 0);
+        clipMask.lineTo(singleCanvasWidth/2 + innerSpacing/2, singleCanvasHeight);
+        clipMask.lineTo(singleCanvasWidth/2 - innerSpacing/2, singleCanvasHeight);
+        clipMask.lineTo(0, 0);
+        clipMask.closePath();
+        context.clip(clipMask);
+
+        //move outwards
+        context.translate(0, singleCanvasHeight/2 - imageSize/2);
+
+        //draw the image
+        context.drawImage(image, singleCanvasWidth/2-imageSize/2, singleCanvasHeight/2-imageSize/2 + imagePositionOffset, imageSize, imageSize);
+
+        //end drawing
+        context.restore();
+    }
 }
+
+function resetCanvasContext(context:CanvasRenderingContext2D, canvasSize:number):void {
+    context.clearRect(0, 0, canvasSize, canvasSize);
+    context.beginPath();
+    context.fillStyle = "#000000";
+    context.fillRect(0,0,canvasSize, canvasSize);
+}
+
+function getInnerSpacing():number {
+    if(spacingSlider == null) return 200;
+
+    return Number(spacingSlider.value);
+}
+
+spacingSlider.addEventListener('input', function(event) {
+    if(spacingValueShow)
+        spacingValueShow.value = "" + getInnerSpacing();
+    else
+        console.log("no textfield found");
+        
+
+    drawImageToCanvases();
+});
+
+function getImageZoom():number {
+    if(!imageZoomSlider)
+        return 600;
+
+    return Number(imageZoomSlider.value);
+}
+
+imageZoomSlider.addEventListener('input', function(event) {
+    drawImageToCanvases();
+});
+
+function getImagePosition():number {
+    if(!imagePosSlider)
+        return 0;
+
+    return Number(imagePosSlider.value);
+}
+
+imagePosSlider.addEventListener('input', function(event) {
+    drawImageToCanvases();
+});
